315e4b7821c9fcd11e99c81f6b60be42668b64d0
matt <mattdxc982@gmail.com>
2019-09-03 16:24:23 +0000

    n/a

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,3 +7,14 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 end
+
+    def create
+    Quote.create(quote_params)
+    redirect_to root_path
+    end
+     private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+end
