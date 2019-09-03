f069985c0e937980bd5c36a30d68d5209f7a3459
matt <mattdxc982@gmail.com>
2019-09-03 16:18:44 +0000

    quote form, got an error-- app/views/layouts/application.html.erb, line 30

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,9 @@
 class QuotesController < ApplicationController
   def index
-  @quote = Quote.order("Random()").first
+    @quote = Quote.order("Random()").first
+  end
+  
+  def new
+    @quote = Quote.new
   end
 end
